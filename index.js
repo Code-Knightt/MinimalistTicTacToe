415b8aed33811db634ad1bd630fd1c1ff177247a
Bhagya Patel <86796273+Code-Knightt@users.noreply.github.com>
2022-04-10 09:10:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let currentPlayer = 1;
 let displayLine = document.getElementById('player');
 let gameOver = false;
+let crossLine = document.getElementById('line');
 
 displayLine.textContent = `Player ${currentPlayer}\'s turn`
 
@@ -53,19 +54,31 @@ function iGotClicked(_id) {
 //Reset Functionality
 document.addEventListener('keypress', (e) => {
     if (e.key.toLowerCase() === 'r') {
-        checkedBoxes = [];
-        player1Boxes = [];
-        player2Boxes = [];
+        reset();
+    }
+})
 
-        let boxes = document.getElementsByClassName('data');
-        for (let index = 0; index < boxes.length; index++) {
-            boxes[index].textContent = '';
-        }
+document.getElementById('reset').addEventListener('click', (e) => {
+    reset();
+})
+
+function reset(){
+    checkedBoxes = [];
+    player1Boxes = [];
+    player2Boxes = [];
+
+    let boxes = document.getElementsByClassName('data');
+    for (let index = 0; index < boxes.length; index++) {
+        boxes[index].textContent = '';
     }
     currentPlayer = 1;
     gameOver = false;
+    crossLine.style.opacity = 0;
+    crossLine.style.margin = 0;
+    crossLine.style.width = '100%';
+    crossLine.style.transform = 'rotate(0deg)';
     displayLine.textContent = `Player ${currentPlayer}\'s turn`
-})
+}
 
 //Winning Functionality
 function isWinner(playerArray, playerNumber) {
@@ -81,7 +94,44 @@ function isWinner(playerArray, playerNumber) {
             displayLine.textContent = `Player ${playerNumber} is winner`;
             winningCombination = combination;
             gameOver = true;
+            drawALine(winningCombination);
+            
             return true
         }
     });
+}
+
+
+//Line functionality
+function drawALine(combination) {
+    let solution = JSON.stringify(combination);
+    
+    crossLine.style.opacity = '1';
+
+    if(solution === JSON.stringify(['0','1','2'])){
+        crossLine.style.marginTop = '16.5%';
+    } else if(solution === JSON.stringify(['3','4','5'])){
+        crossLine.style.marginTop = '49.5%'
+    } else if(solution === JSON.stringify(['6','7','8'])){
+        crossLine.style.marginTop = '82.5%'
+    } else if(solution === JSON.stringify(['0','3','6'])){
+        crossLine.style.marginLeft = '17%'
+        crossLine.style.transform = 'rotate(90deg)';
+    } else if(solution === JSON.stringify(['1','4','7'])){
+        crossLine.style.marginLeft = '51%'
+        crossLine.style.transform = 'rotate(90deg)';
+    } else if(solution === JSON.stringify(['2','5','8'])){
+        crossLine.style.marginLeft = '85%'
+        crossLine.style.transform = 'rotate(90deg)';
+    } else if(solution === JSON.stringify(['0','4','8'])){
+        crossLine.style.width = '110%'
+        crossLine.style.marginLeft = '12%';
+        crossLine.style.marginTop = '11%';
+        crossLine.style.transform = 'rotate(45deg)';
+    } else if(solution === JSON.stringify(['2','4','6'])){
+        crossLine.style.width = '110%'
+        crossLine.style.marginLeft = '90%';
+        crossLine.style.marginTop = '11%';
+        crossLine.style.transform = 'rotate(135deg)';
+    }
 }
